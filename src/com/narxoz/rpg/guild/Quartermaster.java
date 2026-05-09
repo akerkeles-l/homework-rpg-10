8f1fc0f340711f61190e802a5b4e8207f464d5f1
akerkeles-l <akerke.les@narxoz.kz>
2026-05-09 18:55:34 +0500

    implement Quartermaster colleague for supplies management

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -1,21 +1,21 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for gear, supplies, and rewards.
- */
 public class Quartermaster extends GuildMember {
-
     public Quartermaster(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Quartermaster " + getName() + "] Received '" + topic + "' from " + from.getName() + ": " + payload);
+        if (topic.equals("order")) {
+            System.out.println("[Quartermaster " + getName() + "] Preparing supplies for: " + payload);
+        } else if (topic.equals("supplies")) {
+            System.out.println("[Quartermaster " + getName() + "] Checking supply stock for: " + payload);
+        }
     }
 }
