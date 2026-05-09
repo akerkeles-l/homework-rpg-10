39681190c34303f31031509992bbeeec0f3dfa59
akerkeles-l <akerke.les@narxoz.kz>
2026-05-09 18:55:52 +0500

    implement Scout colleague for route reporting and reconnaissance

--- src/com/narxoz/rpg/guild/Scout.java
+++ src/com/narxoz/rpg/guild/Scout.java
@@ -1,21 +1,23 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for route reports and reconnaissance.
- */
 public class Scout extends GuildMember {
-
     public Scout(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void reportRoute(String topic, String payload) {
-        // TODO: send a scouting message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Scout " + getName() + "] Received '" + topic + "' from " + from.getName() + ": " + payload);
+        if (topic.equals("route")) {
+            System.out.println("[Scout " + getName() + "] Mapping route for: " + payload);
+        } else if (topic.equals("recon")) {
+            System.out.println("[Scout " + getName() + "] Gathering intelligence: " + payload);
+        } else if (topic.equals("deploy")) {
+            System.out.println("[Scout " + getName() + "] Scouting ahead for deployment: " + payload);
+        }
     }
 }
