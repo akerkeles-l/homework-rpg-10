40e4c05ec398702715defd6e5628ba8914e30117
akerkeles-l <akerke.les@narxoz.kz>
2026-05-13 13:03:25 +0500

    implement Captain colleague for order issuing and command

--- src/com/narxoz/rpg/guild/Captain.java
+++ src/com/narxoz/rpg/guild/Captain.java
@@ -1,21 +1,23 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for orders and mission coordination.
- */
 public class Captain extends GuildMember {
-
     public Captain(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void issueOrder(String topic, String payload) {
-        // TODO: send a command message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Captain " + getName() + "] Received '" + topic + "' from " + from.getName() + ": " + payload);
+        if (topic.equals("order") || topic.equals("command")) {
+            System.out.println("[Captain " + getName() + "] Acknowledging order and issuing battle commands: " + payload);
+        } else if (topic.equals("route")) {
+            System.out.println("[Captain " + getName() + "] Approving route: " + payload);
+        } else if (topic.equals("broadcast")) {
+            System.out.println("[Captain " + getName() + "] Broadcasting to all troops: " + payload);
+        }
     }
 }
