54d85c95f929d7937963d04534a007ed7597140e
akerkeles-l <akerke.les@narxoz.kz>
2026-05-13 12:49:58 +0500

    implement Healer colleague for recovery and medical aid

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -1,21 +1,23 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for wounds, potions, and recovery plans.
- */
 public class Healer extends GuildMember {
-
     public Healer(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Healer " + getName() + "] Received '" + topic + "' from " + from.getName() + ": " + payload);
+        if (topic.equals("healing")) {
+            System.out.println("[Healer " + getName() + "] Preparing healing potions for: " + payload);
+        } else if (topic.equals("aid")) {
+            System.out.println("[Healer " + getName() + "] Organizing medical aid: " + payload);
+        } else if (topic.equals("casualty")) {
+            System.out.println("[Healer " + getName() + "] Tending to wounded: " + payload);
+        }
     }
 }
